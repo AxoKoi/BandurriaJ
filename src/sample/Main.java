ee2e304ada7cffb1fc7920ef98c5c304684efc93
Ignacio Rondini <jirondini@gmail.com>
2020-08-16 17:00:52 +0200

    With tree view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,9 +2,13 @@ package sample;
 
 import java.io.File;
 
+import org.apache.poi.ss.formula.functions.T;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
@@ -25,9 +29,23 @@ public class Main extends Application {
 			File selectedFile = fileChooser.showOpenDialog(primaryStage);
 		});
 
+		TreeView<String> treeView = new TreeView<>();
+		TreeItem<String> rootItem = new TreeItem<>("Catalogues");
 
+		TreeItem<String> catalogue1 = new TreeItem<>("Catalogue 1");
+		catalogue1.getChildren().add(new TreeItem<>("CD1"));
+		catalogue1.getChildren().add(new TreeItem<>("CD2"));
+
+		TreeItem<String> catalogue2 = new TreeItem<>("Catalogue 2");
+		catalogue2.getChildren().add(new TreeItem<>("CD1"));
+		catalogue2.getChildren().add(new TreeItem<>("CD2"));
+
+		rootItem.getChildren().addAll(catalogue1, catalogue2);
+		treeView.setRoot(rootItem);
 
 		vboxLeft.getChildren().add(button);
+		vboxLeft.getChildren().add(treeView);
+
 		VBox vboxRight = new VBox();
 		vboxRight.getChildren().add(new Button("RightBox"));
 		HBox hbox = new HBox(vboxLeft, vboxRight);
