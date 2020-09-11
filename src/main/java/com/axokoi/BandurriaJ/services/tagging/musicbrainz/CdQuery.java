99e6bf25a55d11a2a372886995ca8ebbe0eafb4e
Ignacio Rondini <jirondini@gmail.com>
2020-09-11 07:31:06 +0200

    [MusicBrainz] First basic failing test.

--- src/main/java/com/axokoi/BandurriaJ/services/tagging/musicbrainz/CdQuery.java
+++ src/main/java/com/axokoi/BandurriaJ/services/tagging/musicbrainz/CdQuery.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.musicbrainz.controller.ReleaseGroup;
 import org.musicbrainz.model.entity.ReleaseWs2;
+import org.musicbrainz.model.searchresult.ReleaseGroupResultWs2;
 import org.springframework.stereotype.Component;
 
 import com.axokoi.BandurriaJ.model.Disc;
@@ -11,8 +12,8 @@ import com.axokoi.BandurriaJ.services.tagging.musicbrainz.converter.CdConverter;
 
 @Component
 public class CdQuery {
-	public static Disc getDiscInfo(String discID) {
-		ReleaseWs2 resultFromMBapi = (ReleaseWs2) callMB(discID);//= use the api
+	public static Disc getDiscInfo(String discName) {
+		ReleaseWs2 resultFromMBapi = callMB(discName);//= use the api
 		CdConverter cdConverter = new CdConverter();
 		return cdConverter.convert(resultFromMBapi);
 	}
@@ -20,7 +21,8 @@ public class CdQuery {
 	private static ReleaseWs2 callMB(String discID) {
 		ReleaseGroup releaseGroup = new ReleaseGroup();
 		releaseGroup.search(discID);
-		List<ReleaseWs2> results = releaseGroup.getFullReleaseList();
+		List<ReleaseGroupResultWs2> apiResult= releaseGroup.getFullSearchResultList();
+		List<ReleaseWs2> results = apiResult.get(0).getReleaseGroup().getReleases();
 		//todo if not empty
 		return results.get(0);
 
