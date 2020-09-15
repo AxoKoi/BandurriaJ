8295e0e693451f4fe5a39e5fe206da41cf016b3f
iro <iro>
2020-09-15 16:01:08 +0200

    [MusicBrainzAPI] Test api call

--- src/main/java/com/axokoi/bandurriaj/services/tagging/musicbrainz/CdQuery.java
+++ src/main/java/com/axokoi/bandurriaj/services/tagging/musicbrainz/CdQuery.java
@@ -3,6 +3,7 @@ package com.axokoi.bandurriaj.services.tagging.musicbrainz;
 import java.util.List;
 
 import org.musicbrainz.controller.ReleaseGroup;
+import org.musicbrainz.model.entity.ReleaseGroupWs2;
 import org.musicbrainz.model.entity.ReleaseWs2;
 import org.musicbrainz.model.searchresult.ReleaseGroupResultWs2;
 import org.springframework.stereotype.Component;
@@ -13,18 +14,17 @@ import com.axokoi.bandurriaj.services.tagging.musicbrainz.converter.CdConverter;
 @Component
 public class CdQuery {
 	public static Disc getDiscInfo(String discName) {
-		ReleaseWs2 resultFromMBapi = callMB(discName);//= use the api
+		ReleaseGroupWs2 resultFromMBapi = callMB(discName);//= use the api
 		CdConverter cdConverter = new CdConverter();
 		return cdConverter.convert(resultFromMBapi);
 	}
 
-	private static ReleaseWs2 callMB(String discID) {
+	private static ReleaseGroupWs2 callMB(String discID) {
 		ReleaseGroup releaseGroup = new ReleaseGroup();
 		releaseGroup.search(discID);
 		List<ReleaseGroupResultWs2> apiResult= releaseGroup.getFullSearchResultList();
-		List<ReleaseWs2> results = apiResult.get(0).getReleaseGroup().getReleases();
-		//todo if not empty
-		return results.get(0);
+		return  apiResult.get(0).getReleaseGroup();
+
 
 	}
 }
--- src/test/java/com/axokoi/bandurriaj/services/tagging/musicbrainz/CdQueryTest.java
+++ src/test/java/com/axokoi/bandurriaj/services/tagging/musicbrainz/CdQueryTest.java
@@ -1,14 +1,17 @@
 package com.axokoi.bandurriaj.services.tagging.musicbrainz;
 
+import com.axokoi.bandurriaj.model.Disc;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CdQueryTest {
 
-	//Non unitary test, as it's really trying to connect to the API.
-	// We use it mainly for development purpose.
-	@Test
-	void findCd() {
-		//	Disc disc = CdQuery.getDiscInfo("AL");
-		//	//assertThat(disc).isNotNull();
-	}
+    //Non unitary test, as it's really trying to connect to the API.
+    // We use it mainly for development purpose.
+    @Test
+    void findCd() {
+        Disc disc = CdQuery.getDiscInfo("Morrison Hotel");
+        assertThat(disc).isNotNull();
+    }
 }
