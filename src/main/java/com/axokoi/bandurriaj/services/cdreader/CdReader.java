9c70592fcef70da6341336ea83f66898ebd54cda
Ignacio Rondini <jirondini@gmail.com>
2020-11-10 07:34:19 +0100

    [Include Read] Fix issue when trying to load several cds. Add JMBDIscId as a maven dependency

--- src/main/java/com/axokoi/bandurriaj/services/cdreader/CdReader.java
+++ src/main/java/com/axokoi/bandurriaj/services/cdreader/CdReader.java
@@ -1,7 +1,7 @@
 package com.axokoi.bandurriaj.services.cdreader;
 
 
-import org.musicbrainz.discid.JMBDiscId;
+import com.axokoi.discid.JMBDiscId;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
