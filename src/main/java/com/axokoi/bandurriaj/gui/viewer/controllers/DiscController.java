2bb8ecc7e62a2e5f075ae124f29ced43c78c0db9
Ignacio Rondini <jirondini@gmail.com>
2021-04-30 13:08:11 +0200

    feat Delete CD - Display Message "disc was deleted."

--- src/main/java/com/axokoi/bandurriaj/gui/viewer/controllers/DiscController.java
+++ src/main/java/com/axokoi/bandurriaj/gui/viewer/controllers/DiscController.java
@@ -1,79 +1,91 @@
 package com.axokoi.bandurriaj.gui.viewer.controllers;
 
-import javax.transaction.Transactional;
-
 import com.axokoi.bandurriaj.gui.commons.PopUpDisplayer;
 import com.axokoi.bandurriaj.gui.commons.popups.DeleteDiscWarningPopupController;
 import com.axokoi.bandurriaj.gui.editor.controllers.DiscEditorController;
 import com.axokoi.bandurriaj.gui.viewer.views.DiscView;
-import javafx.event.ActionEvent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
+import com.axokoi.bandurriaj.i18n.MessagesProvider;
 import com.axokoi.bandurriaj.model.Artist;
 import com.axokoi.bandurriaj.model.Disc;
 import com.axokoi.bandurriaj.services.dataaccess.ArtistService;
 import com.axokoi.bandurriaj.services.dataaccess.DiscService;
-
+import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.transaction.Transactional;
 
 @Component
 public class DiscController extends ViewerController<Disc> {
 
-	@Autowired
-	private DiscView discView;
-	@Autowired
-	private DiscService discService;
-	@Autowired
-	private ArtistService artistService;
-	@Autowired
-	private ArtistController artistController;
-	@Autowired
-	private DeleteDiscWarningPopupController deleteDiscWarningPopupController;
-	@Autowired
-	private  DiscEditorController discEditorController;
-	private final PopUpDisplayer popUpDisplayer;
+   @Autowired
+   private DiscView discView;
+   @Autowired
+   private DiscService discService;
+   @Autowired
+   private ArtistService artistService;
+   @Autowired
+   private ArtistController artistController;
+   @Autowired
+   private CatalogueController catalogueController;
+   @Autowired
+   private DeleteDiscWarningPopupController deleteDiscWarningPopupController;
+   @Autowired
+   private DiscEditorController discEditorController;
 
-	public DiscController( PopUpDisplayer popUpDisplayer) {
-		this.popUpDisplayer = popUpDisplayer;
-	}
+   private final MessagesProvider messagesProvider;
+   private final PopUpDisplayer popUpDisplayer;
 
-	@Override
-	protected Node getView() {
-		return this.discView;
-	}
+   public DiscController(MessagesProvider messagesProvider, PopUpDisplayer popUpDisplayer) {
+      this.messagesProvider = messagesProvider;
+      this.popUpDisplayer = popUpDisplayer;
+   }
 
-	@Override
-	public void refreshView(Disc disc) {
-		discView.refresh(disc);
-	}
+   @Override
+   protected Node getView() {
+      return this.discView;
+   }
 
-	@Transactional
-	public Disc fetchDiscToDisplay(Disc discToDisplay) {
-		return discService.findByBusinessIdentifier(discToDisplay.getBusinessIdentifier()).orElseThrow(()->new RuntimeException("Impossible to retrieve disc from DB:"+discToDisplay.getBusinessIdentifier()));
-	}
+   @Override
+   public void refreshView(Disc disc) {
+      discView.refresh(disc);
+   }
 
-	public Artist fetchArtistToDisplay(Artist artist) {
-		return artistService.findById(artist.getId());
-	}
+   @Transactional
+   public Disc fetchDiscToDisplay(Disc discToDisplay) {
+      return discService.findByBusinessIdentifier(discToDisplay.getBusinessIdentifier()).orElseThrow(() -> new RuntimeException("Impossible to retrieve disc from DB:" + discToDisplay.getBusinessIdentifier()));
+   }
+
+   public Artist fetchArtistToDisplay(Artist artist) {
+      return artistService.findById(artist.getId());
+   }
 
    public void replaceCenterWithArtist(Artist selectedArtist) {
-		artistController.displayViewCenter(selectedArtist);
+      artistController.displayViewCenter(selectedArtist);
 
-	}
+   }
 
-	public void displayEditorPopup(ActionEvent event, Disc disc) {
+   public void displayEditorPopup(ActionEvent event, Disc disc) {
 
-		discEditorController.refreshView(disc);
-		popUpDisplayer.displayNewPopupWithFunction(discEditorController.getView(), this.getView(),
-				() -> {
-			discView.refresh(disc);
-			return null;
-		});
-	}
+      discEditorController.refreshView(disc);
+      popUpDisplayer.displayNewPopupWithFunction(discEditorController.getView(), this.getView(),
+              () -> {
+                 discView.refresh(disc);
+                 return null;
+              });
+   }
 
-	public void displayDeletePopup(ActionEvent event, Disc disc) {
-		deleteDiscWarningPopupController.refreshView(disc);
-		popUpDisplayer.displayNewPopupWithFunction(deleteDiscWarningPopupController.getView(), this.getView(), () -> null);
-	}
+   public void displayDeletePopup(ActionEvent event, Disc disc) {
+      deleteDiscWarningPopupController.refreshView(disc);
+      popUpDisplayer.displayNewPopupWithFunction(deleteDiscWarningPopupController.getView(), this.getView(), () -> {
+         catalogueController.refreshView();
+         var discDeletedLabel = new Label(messagesProvider.getMessageFrom("disc.controller.disc.deleted.successfully", disc.getName()));
+         discDeletedLabel.setFont(new Font(discDeletedLabel.getFont().getFamily(),15));
+         viewDispatcher.replaceCenterWith(discDeletedLabel);
+         return null;
+      });
+   }
 }
